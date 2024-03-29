88e7c5f00b1c855401cedbfc83b8089cc89fc8bc
Pradeep Dilip Mokashi <pradeepmokashi9@gmail.com>
2024-03-29 23:10:33 +0530

    added required field in bloodRequest

--- Models/bloodRequest.js
+++ Models/bloodRequest.js
@@ -1,13 +1,31 @@
 
 const mongoose = require("mongoose")
 const blood_request = new mongoose.Schema({
-          name: String,
-          bloodgrp: String,
-          hospital: String,
-          quantity: String,
-          accepted: Boolean, 
-          rejected: Boolean,
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  bloodgrp: {
+    type: String,
+    required: true,
+  },
+  hospital: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+  accepted: {
+    type: String,
+    required: true,
+  },
+  rejected: {
+    type:String,
+    required:true
+},
+});
 
 const bloodRequest = mongoose.model("bloodRequest",blood_request);
 module.exports = bloodRequest;
